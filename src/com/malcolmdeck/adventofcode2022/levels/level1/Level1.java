99eaa2a3ff9a9a3bee5f0437ca56ea20fc0bbc77
mdeck1 <malcolmdeck@gmail.com>
2022-11-30 21:20:00 -0800

    Add some documentation to the code :)

--- src/com/malcolmdeck/adventofcode2022/levels/level1/Level1.java
+++ src/com/malcolmdeck/adventofcode2022/levels/level1/Level1.java
@@ -17,6 +17,7 @@ public class Level1 {
             while (scanner.hasNextLine()) {
                 int caloriesSoFar = 0;
                 String line = scanner.nextLine();
+                // Iterate over a single elf's calories and sum
                 while (line != "") {
                     int calories = Integer.parseInt(line);
                     caloriesSoFar += calories;
@@ -26,6 +27,7 @@ public class Level1 {
                         break;
                     }
                 }
+                // Maximum is either previous maximum or current elf
                 maxCalories = (maxCalories < caloriesSoFar) ? caloriesSoFar : maxCalories;
             }
             System.out.println("Max Calories: " + maxCalories);
@@ -39,11 +41,13 @@ public class Level1 {
         File file = FileHelper.getFile("level1\\level1data.txt");
         try {
             Scanner scanner = new Scanner(file);
+            // Use PriorityQueue as min Heap; this is the default implementation of a PriorityQueue on Integers, so
+            // no need to use a custom Comparator.
             PriorityQueue<Integer> heap = new PriorityQueue<>();
-            int maxCalories = -1;
             while (scanner.hasNextLine()) {
                 int caloriesSoFar = 0;
                 String line = scanner.nextLine();
+                // Iterate over a single elf's calories and sum
                 while (line != "") {
                     int calories = Integer.parseInt(line);
                     caloriesSoFar += calories;
@@ -53,12 +57,15 @@ public class Level1 {
                         break;
                     }
                 }
+                // Add current elf to min heap.
                 heap.add(caloriesSoFar);
+                // Only want the top 3 values at any given moment, so remove the smallest
                 while (heap.size() > 3) {
                     heap.remove();
                 }
             }
             int topThreeCalories = heap.remove() + heap.remove() + heap.remove();
+            // Check if I screwed up and there are more than 3 values remaining.
             if (heap.size() > 0) {
                 throw new RuntimeException("Heap still has " + heap.size() + " elements.");
             }
