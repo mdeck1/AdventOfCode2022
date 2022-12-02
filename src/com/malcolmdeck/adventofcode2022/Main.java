5ea65105c9bf8d539776d564e4901c472c56bb68
mdeck1 <malcolmdeck@gmail.com>
2022-12-01 21:09:53 -0800

    Initial solution to Day 2.

--- src/com/malcolmdeck/adventofcode2022/Main.java
+++ src/com/malcolmdeck/adventofcode2022/Main.java
@@ -1,10 +1,11 @@
 package com.malcolmdeck.adventofcode2022;
 
 import com.malcolmdeck.adventofcode2022.levels.level1.Level1;
+import com.malcolmdeck.adventofcode2022.levels.level2.Level2;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Level1.partTwo();
+        Level2.partTwo();
     }
 }
--- src/com/malcolmdeck/adventofcode2022/levels/level2/Level2.java
+++ src/com/malcolmdeck/adventofcode2022/levels/level2/Level2.java
@@ -4,7 +4,6 @@ import com.malcolmdeck.adventofcode2022.util.FileHelper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Level2 {
@@ -13,9 +12,51 @@ public class Level2 {
         File file = FileHelper.getFile("level2\\level2data.txt");
         try {
             Scanner scanner = new Scanner(file);
+            int score = 0;
             while (scanner.hasNextLine()) {
-
+                String line = scanner.nextLine();
+                char opponentChoice = line.charAt(0);
+                char myChoice = line.charAt(2);
+                switch (myChoice) {
+                    case 'X':
+                        score += 1;
+                        break;
+                    case 'Y':
+                        score += 2;
+                        break;
+                    case 'Z':
+                        score += 3;
+                        break;
+                    default:
+                        throw new RuntimeException("Unknown char thrown: " + myChoice);
+                }
+                switch (opponentChoice) {
+                    case 'A':
+                        if (myChoice == 'Y') {
+                            score += 6;
+                        } else if (myChoice == 'X') {
+                            score += 3;
+                        }
+                        break;
+                    case 'B':
+                        if (myChoice == 'Z') {
+                            score += 6;
+                        } else if (myChoice == 'Y') {
+                            score += 3;
+                        }
+                        break;
+                    case 'C':
+                        if (myChoice == 'X') {
+                            score += 6;
+                        } else if (myChoice == 'Z') {
+                            score += 3;
+                        }
+                        break;
+                    default:
+                        throw new RuntimeException("Unknown char thrown: " + myChoice);
+                }
             }
+            System.out.println("Score: " + score);
         } catch (Exception e) {
             System.out.println("FileNotFound: " + e.getMessage());
         }
@@ -26,10 +67,58 @@ public class Level2 {
         File file = FileHelper.getFile("level2\\level2data.txt");
         try {
             Scanner scanner = new Scanner(file);
+            int score = 0;
             while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                char opponentChoice = line.charAt(0);
+                char myChoice = line.charAt(2);
+                switch (myChoice) {
+                    case 'X':
+                        score += 0;
+                        break;
+                    case 'Y':
+                        score += 3;
+                        break;
+                    case 'Z':
+                        score += 6;
+                        break;
+                    default:
+                        throw new RuntimeException("Unknown char thrown: " + myChoice);
+                }
+                switch (opponentChoice) {
+                    case 'A':
+                        if (myChoice == 'X') {
+                            score += 3;
+                        } else if (myChoice == 'Y') {
+                            score += 1;
+                        } else {
+                            score += 2;
+                        }
+                        break;
+                    case 'B':
+                        if (myChoice == 'X') {
+                            score += 1;
+                        } else if (myChoice == 'Y') {
+                            score += 2;
+                        } else {
+                            score += 3;
+                        }
+                        break;
+                    case 'C':
+                        if (myChoice == 'X') {
+                            score += 2;
+                        } else if (myChoice == 'Y') {
+                            score += 3;
+                        } else {
+                            score += 1;
+                        }
+                        break;
+                    default:
+                        throw new RuntimeException("Unknown char thrown: " + myChoice);
+                }
             }
-
-        } catch (FileNotFoundException e) {
+            System.out.println("Score: " + score);
+        } catch (Exception e) {
             System.out.println("FileNotFound: " + e.getMessage());
         }
         return;
