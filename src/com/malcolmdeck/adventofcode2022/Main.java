f3e5cb9656e3bc852e1bf6db1804dc2b723b1845
mdeck1 <malcolmdeck@gmail.com>
2022-12-09 20:54:59 -0800

    Day 10 Skeleton

--- src/com/malcolmdeck/adventofcode2022/Main.java
+++ src/com/malcolmdeck/adventofcode2022/Main.java
@@ -1,6 +1,7 @@
 package com.malcolmdeck.adventofcode2022;
 
 import com.malcolmdeck.adventofcode2022.levels.level1.Level1;
+import com.malcolmdeck.adventofcode2022.levels.level10.Level10;
 import com.malcolmdeck.adventofcode2022.levels.level2.Level2;
 import com.malcolmdeck.adventofcode2022.levels.level3.Level3;
 import com.malcolmdeck.adventofcode2022.levels.level4.Level4;
@@ -13,6 +14,6 @@ import com.malcolmdeck.adventofcode2022.levels.level9.Level9;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Level9.partTwo();
+        Level10.partOne();
     }
 }
