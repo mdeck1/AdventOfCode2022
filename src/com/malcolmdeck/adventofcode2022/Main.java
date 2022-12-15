ffc1168befda27899404a57ea826157df8584a8f
mdeck1 <malcolmdeck@gmail.com>
2022-12-14 20:56:41 -0800

    Skeletons for Days 15-25

--- src/com/malcolmdeck/adventofcode2022/Main.java
+++ src/com/malcolmdeck/adventofcode2022/Main.java
@@ -6,6 +6,7 @@ import com.malcolmdeck.adventofcode2022.levels.Level11.Level11;
 import com.malcolmdeck.adventofcode2022.levels.level12.Level12;
 import com.malcolmdeck.adventofcode2022.levels.level13.Level13;
 import com.malcolmdeck.adventofcode2022.levels.level14.Level14;
+import com.malcolmdeck.adventofcode2022.levels.level15.Level15;
 import com.malcolmdeck.adventofcode2022.levels.level2.Level2;
 import com.malcolmdeck.adventofcode2022.levels.level3.Level3;
 import com.malcolmdeck.adventofcode2022.levels.level4.Level4;
@@ -18,6 +19,6 @@ import com.malcolmdeck.adventofcode2022.levels.level9.Level9;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Level14.partTwo();
+        Level15.partOne();
     }
 }
---
+++ src/com/malcolmdeck/adventofcode2022/levels/level23/Level23.java
@@ -0,0 +1,38 @@
+package com.malcolmdeck.adventofcode2022.levels.level23;
+
+import com.malcolmdeck.adventofcode2022.util.FileHelper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Level23 {
+
+    public static void partOne() throws Exception {
+        File file = FileHelper.getFile("level15\\level15data.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+
+            }
+            System.out.println(": " );
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFound: " + e.getMessage());
+        }
+        return;
+    }
+
+    public static void partTwo() throws Exception {
+        File file = FileHelper.getFile("level15\\level15data.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+
+            }
+            System.out.println(": " );
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFound: " + e.getMessage());
+        }
+        return;
+    }
+}
